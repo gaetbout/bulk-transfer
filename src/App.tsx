876a4790b83e8241a2cf1e48181e17332c0be207
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-11-29 19:15:00 +0300

    Starknet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
+import { connect, disconnect, StarknetWindowObject } from "get-starknet";
+import { useEffect, useState } from "react";
 import Alert from "react-bootstrap/Alert";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
-import { useAccount, useConnect, useDisconnect, useNetwork, useProvider } from "wagmi";
-import { prepareSendTransaction, sendTransaction } from "@wagmi/core";
-import { InjectedConnector } from "wagmi/connectors/injected";
 import { parse } from "papaparse";
 import { ethers } from "ethers";
+import { Call, CallData, RpcProvider, uint256 } from "starknet";
 import "./App.css";
 
 const papaparseOptions = {
@@ -26,20 +25,43 @@ interface ILine {
   hash?: string;
 }
 
-const erc20Abi = new ethers.utils.Interface([
-  "function decimals() view returns (uint256)",
-  "function transfer(address _to, uint256 _value)",
-]);
-
 function App() {
   const [lines, setLines] = useState<ILine[]>();
+  const [status, setStatus] = useState<string>();
   const [error, setError] = useState<string>();
+  const [wallet, setWallet] = useState<StarknetWindowObject>();
+  const [provider, setProvider] = useState<RpcProvider>();
 
-  const { address, isConnected } = useAccount();
-  const { connect } = useConnect({ connector: new InjectedConnector() });
-  const { disconnect } = useDisconnect();
-  const { chain } = useNetwork();
-  const provider = useProvider();
+  wallet?.on("networkChanged", (accounts: string[]) => {
+    console.log("networkChanged", accounts);
+  });
+
+  async function connectWallet() {
+    const wallet = await connect({ include: ["argentX"] });
+    if (!wallet) {
+      return;
+    }
+    let nodeUrl = "";
+    setWallet(wallet);
+    if (wallet.chainId === "SN_MAIN") {
+      nodeUrl = "https://starknet-mainnet.infura.io/v3/76fac2f9c85a49878a233e624acd14d5";
+    } else if (wallet.chainId === "SN_GOERLI") {
+      nodeUrl = "https://starknet-goerli.infura.io/v3/76fac2f9c85a49878a233e624acd14d5";
+    }
+    setProvider(new RpcProvider({ nodeUrl }));
+  }
+
+  useEffect(() => {
+    if (!wallet) return;
+    const handler = async (accounts: string[]) => {
+      console.log("networkChanged", accounts);
+      await connectWallet();
+    };
+    wallet.on("networkChanged", handler);
+    return () => {
+      wallet.off("networkChanged", handler);
+    };
+  }, [wallet]);
 
   const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
@@ -55,65 +77,56 @@ function App() {
     reader.readAsText(files[0], papaparseOptions.fileEncoding);
   };
 
-  const handleSend = async (line: ILine) => {
-    const index = lines?.findIndex((l) => l === line);
-    // const line = lines?.[sendIndex];
-    console.log("handleSend", line);
-    setLines((lines) => lines?.map((l, i) => (i === index ? { ...l, status: "Loading", hash: undefined } : l)));
+  const handleSend = async (lines: ILine[]) => {
+    setStatus(undefined);
+
+    const promises = lines.map(async ({ ToAddress, Token, TokenAddress, Amount }) => {
+      const { result } = await provider!.callContract({ contractAddress: TokenAddress, entrypoint: "decimals" });
+      const [decimals] = result;
+      console.log("decimals for ", Token, "->", decimals);
+      const amount = ethers.utils.parseUnits(Amount, decimals).toString();
+      return {
+        contractAddress: TokenAddress,
+        entrypoint: "transfer",
+        calldata: CallData.compile([ToAddress, uint256.bnToUint256(amount)]),
+      };
+    });
+
+    const calls: Call[] = await Promise.all(promises);
+
+    console.log("handleSend", { lines, calls });
+    // setLines((lines) => lines?.map((l, i) => (i === index ? { ...l, status: "Loading", hash: undefined } : l)));
     try {
-      let request;
-      if (line.Token === "ETH") {
-        request = { to: line.ToAddress, value: ethers.utils.parseEther(line.Amount) };
-      } else {
-        const erc20 = new ethers.Contract(line.TokenAddress, erc20Abi, provider);
-        const decimals = await erc20.decimals();
-        console.log("decimals", decimals);
-        const amount = ethers.utils.parseUnits(line.Amount, decimals);
-        const data = erc20Abi.encodeFunctionData("transfer", [line.ToAddress, amount]);
-        request = { to: line.TokenAddress, data };
-      }
-      const config = await prepareSendTransaction({ request });
-      const { hash, wait } = await sendTransaction(config);
-      setLines((lines) => lines?.map((l, i) => (i === index ? { ...l, status: "Pending", hash } : l)));
-      console.log("hash", hash);
-      const receipt = await wait();
-      setLines((lines) => lines?.map((l, i) => (i === index ? { ...l, status: "Done" } : l)));
-      console.log("receipt", receipt);
+      const { transaction_hash } = await wallet?.account.execute(calls);
+      setStatus(`Transaction sent: ${transaction_hash}`);
+      await wallet?.provider.waitForTransaction(transaction_hash);
+      setStatus(`Confirmed: ${transaction_hash}`);
     } catch (e) {
-      setLines((lines) => lines?.map((l, i) => (i === index ? { ...l, status: "Error (see below)" } : l)));
+      // setLines((lines) => lines?.map((l, i) => (i === index ? { ...l, status: "Error (see below)" } : l)));
       setError(`${e}`);
       throw e;
     }
   };
 
-  const hashLink = (hash: string | undefined) => {
-    if (!hash) return "";
-    const subdomain = chain?.name === "Goerli" ? "goerli." : "";
-    const href = `https://${subdomain}etherscan.io/tx/${hash}`;
-    return (
-      <a href={href} target="_blank" rel="noreferrer">
-        {hash.slice(0, 2)}...{hash.slice(-4)}
-      </a>
-    );
-  };
-
-  if (!isConnected) {
+  if (!wallet) {
     return (
       <div className="App mt-3">
-        <Button onClick={() => connect()}>Connect wallet</Button>
+        <Button onClick={connectWallet}>Connect wallet</Button>
       </div>
     );
   }
 
+  const address = wallet.account.address;
   return (
     <div className="App mt-3 px-3">
       <div className="fw-light">
-        Connected {address!.slice(0, 6)}...{address!.slice(-4)} to {chain?.name}.{" "}
+        Connected {address!.slice(0, 6)}...{address!.slice(-4)} to {wallet.chainId}.{" "}
         <a
           href="/"
-          onClick={(e) => {
+          onClick={async (e) => {
             e.preventDefault();
-            disconnect();
+            await disconnect();
+            setWallet(undefined);
           }}
         >
           Disconnect
@@ -132,8 +145,6 @@ function App() {
                 <th>Token</th>
                 <th>TokenAddress</th>
                 <th>Amount</th>
-                <th>Send</th>
-                <th>Status</th>
               </tr>
             </thead>
             <tbody>
@@ -148,16 +159,12 @@ function App() {
                     {line.TokenAddress}
                   </td>
                   <td className="align-middle">{line.Amount}</td>
-                  <td>
-                    <Button onClick={() => handleSend(line)}>Send</Button>
-                  </td>
-                  <td className={`align-middle ${line.status === "Done" ? "text-success" : ""}`}>
-                    {line.status || ""} {hashLink(line.hash)}
-                  </td>
                 </tr>
               ))}
             </tbody>
           </Table>
+          <Button onClick={() => handleSend(lines)}>Send</Button>
+          {status && <Alert variant="info">{status}</Alert>}
           {error && <Alert variant="danger">{error}</Alert>}
         </>
       )}
